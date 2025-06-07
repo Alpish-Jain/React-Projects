d0cc2932347d2418d24381ddf3b73ec021f04205
Alpish-Jain <46166671+Alpish-Jain@users.noreply.github.com>
2025-06-07 14:58:32 +0530

    Configuring  redux for the app

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
+import {login,logout } from "./store/authSlice"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => { 
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({userData}))
+        } else {
+          dispatch(logout())
+        }
+      }).catch((error)=>console.log("error in user authentication"))
+      .finally(()=>setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap
+  content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+  </div>
+  ): null;
 }
 
 export default App
